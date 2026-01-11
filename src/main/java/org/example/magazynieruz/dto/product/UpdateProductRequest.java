939d1019bb8934034b846169b4dcbfc39164aab8
Gracjan <106824179+gracjanStrak@users.noreply.github.com>
2026-01-11 10:44:53 +0100

    stashed classes cleanup

--- src/main/java/org/example/magazynieruz/dto/product/UpdateProductRequest.java
+++
@@ -1,8 +0,0 @@
-package org.example.magazynieruz.dto.product;
-
-public record UpdateProductRequest(
-        String name,
-        String description,
-        Double price,
-        Integer quantity
-){}
